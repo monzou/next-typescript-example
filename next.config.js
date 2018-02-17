0456a482d63030798d60ecf16b66e25b08f6e616
monzou <1984tkr@gmail.com>
2018-02-17 15:24:10 +0900

    Use next-bundle-analyzer plugin

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 const withTs = require('@zeit/next-typescript')
 const withSass = require('@zeit/next-sass')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 const { ANALYZE } = process.env
 
-module.exports = withTs(withSass({
-  webpack: function (config, { isServer }) {
-    if (ANALYZE) {
-      config.plugins.push(new BundleAnalyzerPlugin({
-        analyzerMode: 'server',
-        analyzerPort: isServer ? 8888 : 8889,
-        openAnalyzer: true
-      }))
-    }
-    return config
-  }
-}))
+module.exports = withTs(withSass(withBundleAnalyzer({
+  analyzeServer: ['server', 'all'].includes(ANALYZE),
+  analyzeBrowser: ['browser', 'all'].includes(ANALYZE)
+})))
